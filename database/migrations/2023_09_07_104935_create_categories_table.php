c4b4ba654d9103c1ef58e065f8fb6f077afdfa61
twiceM <mohamed98mahmoud2016@gmail.com>
2023-09-09 17:44:01 +0300

    init category (model-migration-seeder-resource)

---
+++ database/migrations/2023_09_07_104935_create_categories_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->bigIncrements("id");
+            $table->string('name',255)->unique();
+            $table->unsignedBigInteger('parent_id')->nullable();
+            $table->timestamps();
+            //create foreign id between parent and child category
+            $table->foreign("parent_id")->references("id")->on("categories")->onDelete("cascade");
+        });
+
+
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+};
---
+++ database/seeders/CategoriesSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Seeder;
+
+class CategoriesSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $count = 5;
+        //Seed main categories
+        try {
+            $categories = [];
+            for ($i = 1; $i <= $count;$i++) {
+                $categories[] = (new Category())->create(["name"=>"Category ".$i ,"parent_id" => NULL]);
+            }
+            $this->addCategoriesToParent($categories,$count);
+        } catch (\Exception $e) {
+
+        }
+
+
+    }
+
+    private function addCategoriesToParent($categories,$count,$levels = 5) {
+        if ($levels < 0) {
+            return;
+        }
+
+        foreach ($categories as $category) {
+            $categoriesChild = [];
+            for ($i = 1; $i <= $count;$i++) {
+
+                $categoriesChild[] = (new Category())->create(["name"=>"Sub Category ".$category->id . "(".$i .")" ,"parent_id" => $category->id]);
+            }
+
+            $this->addCategoriesToParent($categoriesChild,$count,$levels - 1);
+
+        }
+    }
+}
